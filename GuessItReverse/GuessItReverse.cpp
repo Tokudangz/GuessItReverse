d679c2f65b8a8b63c09ed6f4d46ffbf2d5cb7205
Tokudangz <avatarstar2003@gmail.com>
2022-03-29 09:42:49 +0700

    update

--- GuessItReverse/GuessItReverse.cpp
+++ GuessItReverse/GuessItReverse.cpp
@@ -40,7 +40,8 @@ void handleInput();
 void showInput();
 void RenderText(std::string output, int h);
 
-int main(int argc, char* args[]) {
+int main(int argc, char* args[])
+{
 	init();
 	while (!is_quit)
 	{
@@ -50,6 +51,7 @@ int main(int argc, char* args[]) {
 		showInput();
 		SDL_RenderPresent(screen);
 	}
+
 	SDL_DestroyRenderer(screen);
 	SDL_DestroyWindow(window);
 	TTF_Quit();
@@ -57,7 +59,8 @@ int main(int argc, char* args[]) {
 	return 0;
 }
 
-void init() {
+void init()
+{
 	if (SDL_Init(SDL_INIT_VIDEO) < 0)
 	{
 		std::cout << SDL_GetError() << std::endl;
@@ -77,25 +80,29 @@ void init() {
 	}
 
 	screen = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC);
-	if (screen == NULL) {
+	if (screen == NULL)
+	{
 		std::cout << SDL_GetError() << std::endl;
 		exit(1);
 	}
 
 	myFont = TTF_OpenFont("8-BIT-WONDER.ttf", 24);
-	if (myFont == NULL) {
+	if (myFont == NULL)
+	{
 		std::cout << TTF_GetError() << std::endl;
 		return;
 	}
 }
 
-void RenderText(std::string output, int h) {
+void RenderText(std::string output, int h)
+{
 	SDL_Surface* LoadSurface = TTF_RenderText_Solid(myFont, output.c_str(), color);
 	if (LoadSurface == NULL)
 	{
 		std::cout << SDL_GetError() << std::endl;
 		return;
 	}
+
 	textHeight = LoadSurface->h;
 	textWidth = LoadSurface->w;
 
@@ -105,22 +112,32 @@ void RenderText(std::string output, int h) {
 		std::cout << SDL_GetError() << std::endl;
 		return;
 	}
+
 	SDL_FreeSurface(LoadSurface);
-	SDL_Rect renderQuad = { SCREEN_WIDTH / 2 - textWidth / 2, h , textWidth, textHeight };
+	SDL_Rect renderQuad = { SCREEN_WIDTH / 2 - textWidth / 2, h, textWidth, textHeight
+	};
 	SDL_RenderCopy(screen, newTexture, NULL, &renderQuad);
 }
 
-void handleInput() {
-	while (SDL_PollEvent(&event) != 0) {
-		if (!gameOver) {
+void handleInput()
+{
+	while (SDL_PollEvent(&event) != 0)
+	{
+		if (!gameOver)
+		{
 			digit = -1;
-			if (event.type == SDL_QUIT) {
+			if (event.type == SDL_QUIT)
+			{
 				gameOver = true;
 				is_quit = true;
 			}
-			if (event.type == SDL_KEYDOWN) {
-				if (choosing) {
-					switch (event.key.keysym.sym) {
+
+			if (event.type == SDL_KEYDOWN)
+			{
+				if (choosing)
+				{
+					switch (event.key.keysym.sym)
+					{
 					case SDLK_0:
 						digit = 0;
 						break;
@@ -158,70 +175,80 @@ void handleInput() {
 					case SDLK_RETURN:
 						choosing = false;
 					}
-					if (digit != -1) {
+
+					if (digit != -1)
+					{
 						choose = choose * 10 + digit;
 						renderChoose = std::to_string(choose);
 					}
 				}
 			}
+
 			if (event.type == SDL_MOUSEBUTTONDOWN)
 			{
 				guess = (first + last) / 2;
-				if (guessing) {
-					if (event.button.button == SDL_BUTTON_LEFT) {
+				if (guessing)
+				{
+					if (event.button.button == SDL_BUTTON_LEFT)
+					{
 						renderGuess = std::to_string(guess);
-						if (guess == choose) {
+						if (guess == choose)
+						{
 							guessing = false;
 							renderResult = "Correct";
-							correct = true;
 							gameOver = true;
+							score++;
 						}
-						else if (choose > guess) {
+						else if (choose > guess)
+						{
 							first = guess;
+							score++;
 						}
-						else {
+						else
+						{
 							last = guess;
+							score++;
 						}
-					}
-					else if (event.type == SDL_MOUSEBUTTONDOWN && event.button.button == SDL_BUTTON_LEFT) {
-						guessing = true;
-						score++;
-						guess = (first + last )/ 2;
 					}
 				}
 			}
 		}
-		else {
-			if (event.type == SDL_KEYDOWN && event.key.keysym.sym == SDLK_RETURN)
+		else
+		{
+			if (event.type == SDL_KEYDOWN)
 			{
-				renderResult = "PLAY AGAIN";
-				renderGuess = "Y       N";
-				renderPick = "Computer Guess Count";
-				renderChoose = std::to_string(score);
-				if (event.type == SDL_KEYDOWN) {
-					if (event.key.keysym.sym == SDLK_y) {
-						gameOver = false;
-						renderPick = "Pick A Number";
-						renderGuess = "0";
-						renderResult = "Computer Guess";
-						choosing = true;
-						choose = 0;
-						first = 1;
-						last = 100;
-						guess = 0;
-						guessing = true;
-						score = 0;
-					}
-					else if (event.key.keysym.sym == SDLK_n || event.type == SDL_QUIT) {
-						gameOver = true;
-						is_quit = true;
-					}
+				if (event.key.keysym.sym == SDLK_RETURN)
+				{
+					renderResult = "PLAY AGAIN";
+					renderGuess = "Y       N";
+					renderPick = "Computer Guess Count";
+					renderChoose = std::to_string(score);
+				}
+
+				if (event.key.keysym.sym == SDLK_y)
+				{
+					gameOver = false;
+					renderPick = "Pick A Number";
+					renderGuess = "0";
+					renderResult = "Computer Guess";
+					renderChoose = "0";
+					choosing = true;
+					choose = 0;
+					first = 1;
+					last = 100;
+					guess = 0;
+					guessing = true;
+					score = 0;
+				}
+				else if (event.key.keysym.sym == SDLK_n || event.type == SDL_QUIT)
+				{
+					gameOver = true;
+					is_quit = true;
 				}
 			}
 		}
 	}
 }
-
 
 void showInput()
 {
